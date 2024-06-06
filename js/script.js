7c7264ce85e8d434d8244fd3665944b06461bb75
Spedine <tucogamesytthegamesof@gmail.com>
2024-06-05 21:28:34 -0300

    feat: intregração web chat

--- js/script.js
+++ js/script.js
@@ -16,3 +16,17 @@ document.body.addEventListener('click', (e) => {
 document.querySelector("#menuBox").addEventListener("click", () => {
     showMenu()
 })
+
+// watson
+
+window.watsonAssistantChatOptions = {
+    integrationID: "85f06573-9891-474a-9baa-92de6c0ec78f", // The ID of this integration.
+    region: "us-south", // The region your integration is hosted in.
+    serviceInstanceID: "69585cf6-59a4-4ef7-896c-7d6d171cf223", // The ID of your service instance.
+    onLoad: async (instance) => { await instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });
